11a2fea500987f9c3ddf2cc132827c82091eab85
bastienroque <roquembastien@gmail.com>
2026-06-09 11:28:13 +0100

    Update missing padding

--- app/my-stations/page.tsx
+++ app/my-stations/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 const LikedStations = async () => {
   return (
-    <div className="flex flex-col gap-6 items-start">
+    <div className="flex flex-col gap-6 items-start pb-24">
       <div className="flex flex-col gap-1 items-start">
         <h1 className="text-2xl font-semibold tracking-tight">My Stations</h1>
         <p>You can find and update your favorite stations right here.</p>
